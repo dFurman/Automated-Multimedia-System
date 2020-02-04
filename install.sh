05d5165ba56906dedb28afa58da1675fd91faea1
Furman <david.fur91@gmail.com>
2020-02-05 00:54:28 +0200

    added sonarr api

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ echo **Spin up containers**
 docker-compose up -d
 
 echo **Get Radarr APIkey**
-radarrAPIKey=($(grep -oP '(?<=ApiKey>)[^<]+' "$SHARE_LOCATION/htpc/radarr/config/config.xml"))
+RadarrAPIKey=($(grep -oP '(?<=ApiKey>)[^<]+' "$SHARE_LOCATION/htpc/radarr/config/config.xml"))
 echo $radarrAPIKey
+
+echo **Get Sonarr APIkey**
+SonarrAPIKey=($(grep -oP '(?<=ApiKey>)[^<]+' "$SHARE_LOCATION/htpc/sonarr/config/config.xml"))
+echo $SonarrAPIKey
 
