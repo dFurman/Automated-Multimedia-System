f609f8f6335e305a20f46d1b8ee728b22429d8dd
Furman <david.fur91@gmail.com>
2020-02-05 01:34:29 +0200

    updated install

--- install.sh
+++ install.sh
@@ -4,10 +4,21 @@ echo **Spin up containers**
 docker-compose up -d
 
 echo **Get Radarr APIkey**
-RadarrAPIKey=($(grep -oP '(?<=ApiKey>)[^<]+' "$SHARE_LOCATION/htpc/radarr/config/config.xml"))
+echo "Waiting for Radarr to be up..."
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:7878)" != "200" ]]
+do sleep 5
+done
+# RadarrAPIKey=($(grep -oP '(?<=ApiKey>)[^<]+' "$SHARE_LOCATION/htpc/radarr/config/config.xml"))
+RadarrAPIKey=(curl localhost:7878 | grep ApiKey | cut -d"'" -f 2)
 echo $radarrAPIKey
 
 echo **Get Sonarr APIkey**
-SonarrAPIKey=($(grep -oP '(?<=ApiKey>)[^<]+' "$SHARE_LOCATION/htpc/sonarr/config/config.xml"))
+echo "Waiting for Sonarr to be up..."
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8989)" != "200" ]]
+do sleep 5
+done
+
+# SonarrAPIKey=($(grep -oP '(?<=ApiKey>)[^<]+' "$SHARE_LOCATION/htpc/sonarr/config/config.xml"))
+SonarrAPIKey=(curl localhost:8989 | grep ApiKey | cut -d"'" -f 2)
 echo $SonarrAPIKey
 
